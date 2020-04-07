44d1a53bf51e70daa27d8ddc790315f169528d24
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-04-07 23:57:00 +0200

    ajout exo_07_c

---
+++ exo_07_c.rb
@@ -0,0 +1,7 @@
+user_name = gets.chomp 
+puts user_name
+
+#Le terminal ne propose pas d'action. 
+#Le programme exo_07_c est le plus clair pour l'utilisateur 
+#car il affiche clairement à la fois la saisie de l'utilisateur 
+#et l'ouptut dans le terminal 
