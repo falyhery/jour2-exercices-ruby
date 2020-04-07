3aaf8aacc050060ef44058dc4694fe9fc3d1ff53
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-04-07 23:55:53 +0200

    ajout exo_07_b

---
+++ exo_07_b.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp 
+puts user_name
+
+#Le terminal demande de saisir un nom
+#Ensuite, le programme le stock dans la variable user_name et l'affiche 
