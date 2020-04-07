3ab2b2ca8ee651b6a5548b2b95e68ff1d63eed66
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-04-07 23:54:36 +0200

    ajout exo_07_a

---
+++ exo_07_a.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+#gets.chomp permet de garder la salutaiton sur une seule ligne 
+#au lieu de la séparer en deux dans le cas où la salutation 
+#se trouverait entre des accolades
