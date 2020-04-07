ade18e18d186f4cbba19ae311331161f1c169f52
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-04-08 00:16:52 +0200

    ajout exo_09

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Quel est ton prénom ?"
+print "> " 
+first_name = gets.chomp
+puts "Quel est ton nom de famille ?"
+print "> "
+last_name = gets.chomp 
+puts "Bonjour, #{first_name} #{last_name} !"
